c3f1bbb806f55794cdf45b7134ff0b482648dc34
Karthic Raghupathi <karthicr@gmail.com>
2019-11-17 01:17:34 -0500

    Setup Test Rig. Yet To Write Tests.

---
+++ sql_bootstrap/test/test_mongo.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestMongoBackend(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_sample(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
